ea83a44608466640195eb3d91d30d55ad17b7e0b
john <john.davis.2015.03@gmail.com>
2021-12-30 17:09:44 -0500

    LoginScreen y Navbar

--- src/index.js
+++ src/index.js
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { CalendarApp } from './CalendarApp';
 
-
 ReactDOM.render(<CalendarApp />, document.getElementById('root'));
-
-
-
-
-
