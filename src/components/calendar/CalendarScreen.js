fc2bd7469f8bee30892070b56e09423cd2a57057
john <john.davis.2015.03@gmail.com>
2021-12-31 12:54:24 -0500

    Creando un modal sobre el calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -6,6 +6,7 @@ import moment from "moment";
 import { Navbar } from "../ui/Navbar";
 import { messages } from "../../helpers/calendar-messages-es";
 import { CalendarEvent } from "./CalendarEvent";
+import { CalendarModal } from "./CalendarModal";
 
 import "moment/locale/es";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -81,6 +82,8 @@ export const CalendarScreen = () => {
           event: CalendarEvent,
         }}
       />
+
+      <CalendarModal />
     </div>
   );
 };
