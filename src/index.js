7c5004bbddfb427614cfde80534060e790879a07
john <john.davis.2015.03@gmail.com>
2021-12-30 19:47:38 -0500

    React Big Calendar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { CalendarApp } from './CalendarApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import { CalendarApp } from "./CalendarApp";
 
-ReactDOM.render(<CalendarApp />, document.getElementById('root'));
+import "./styles.css";
+
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
