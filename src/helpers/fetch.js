e0d5db9c157c8f2bb15602828635f2e4b94f4680
john <john.davis.2015.03@gmail.com>
2022-04-19 20:12:42 -0500

    Pruebas unitarias y de integración - MERN Clase 397

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -3,6 +3,8 @@ const baseUrl = process.env.REACT_APP_API_URL;
 const fetchSinToken = (endpoint, data, method = "GET") => {
   const url = `${baseUrl}/${endpoint}`; //localhost:4000/api/auth
 
+  console.log(url);
+
   if (method === "GET") {
     return fetch(url);
   } else {
